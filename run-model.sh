230a9f8940ca12aaa211e4b7443f4b6da9f1daad
Daniel Shen <dshen109@gmail.com>
2025-11-29 09:51:23 -0500

    add in emissions intensity calculations

--- run-model.sh
+++ run-model.sh
@@ -1,5 +1,5 @@
 # rm -r out-query || true
-mcdp-solve-query  --optimistic 20 --pessimistic 20 tariff_query_fun --imp
+mcdp-solve-query  --optimistic 20 --pessimistic 20 tariff_query_fun --imp --nocache
 
 # mcdp-check-circle   mcdp-eval           mcdp-load-all       mcdp-render         mcdp-solve          mcookie
 # mcdp-depgraph       mcdp-export         mcdp-plot           mcdp-render-manual  mcdp-solve-query
